0163ccd1913dd692fa56207133004e3beab4d9a8
Marvin Altemeier <marv.internetz@gmail.com>
2016-08-11 20:05:41 +0200

    Added testcases for pizza constructor

---
+++ src/CityTests/CityTest.cs
@@ -0,0 +1,79 @@
+﻿using Xunit;
+
+namespace CityTests
+{
+    public class CityTest
+    {
+        [Fact]
+        public void CityConstructorTest1Width()
+        {
+            int expectedWidth = 4;
+
+            City.City c = new City.City(4, 5);
+            Assert.Equal(expectedWidth, c.Width);
+        }
+
+        [Fact]
+        public void CityConstructorTest1Height()
+        {
+            int expectedHeight = 5;
+
+            City.City c = new City.City(4, 5);
+            Assert.Equal(expectedHeight, c.Height);
+        }
+
+        [Fact]
+        public void CityConstructorTest2Width()
+        {
+            int expectedWidth = 4;
+
+            City.City c = new City.City(4);
+            Assert.Equal(expectedWidth, c.Width);
+        }
+
+        [Fact]
+        public void CityConstructorTest2Height()
+        {
+            int expectedHeight = 4;
+
+            City.City c = new City.City(4);
+            Assert.Equal(expectedHeight, c.Height);
+        }
+
+        [Fact]
+        public void CityConstructorTest3XWidth()
+        {
+            int expectedWidth = 4;
+
+            City.City c = new City.City("4x5");
+            Assert.Equal(expectedWidth, c.Width);
+        }
+
+        [Fact]
+        public void CityConstructorTest3XHeight()
+        {
+            int expectedHeight = 5;
+
+            City.City c = new City.City("4x5");
+            Assert.Equal(expectedHeight, c.Height);
+        }
+
+        [Fact]
+        public void CityConstructorTest3StarWidth()
+        {
+            int expectedWidth = 4;
+
+            City.City c = new City.City("4*5");
+            Assert.Equal(expectedWidth, c.Width);
+        }
+
+        [Fact]
+        public void CityConstructorTest3StarHeight()
+        {
+            int expectedHeight = 5;
+
+            City.City c = new City.City("4*5");
+            Assert.Equal(expectedHeight, c.Height);
+        }
+    }
+}

c32a0fac0455f5f3df4f3237f3fe71741680b428
Marvin Altemeier <marv.internetz@gmail.com>
2016-08-11 20:08:14 +0200

    Removed redundant entry

--- src/Pizza/Data/ApplicationDbContext.cs
+++ src/Pizza/Data/ApplicationDbContext.cs
@@ -22,7 +22,6 @@ namespace Pizza.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
-
-        public DbSet<City> City { get; set; }
+        
     }
 }

f5f97875b4bc845e02daed53661e187e49a54ae7
Marvin Altemeier <marv.internetz@gmail.com>
2016-08-11 19:38:57 +0200

    Added project for cities

---
+++ src/City/City.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace City
+{
+    public class City
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Constructor taking height and width of city
+        /// </summary>
+        /// <param name="height">Height of the map</param>
+        /// <param name="width">Width of the map</param>
+        public City(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        /// <summary>
+        /// Constructor taking only one parameter which translates to width and height
+        /// </summary>
+        /// <param name="dimensions">Parameter defining width and height</param>
+        public City(int dimensions) : this(dimensions, dimensions)
+        { }
+
+        /// <summary>
+        /// Constructor taking string containing seperator. Supports '*' and 'x' as seperator. Example string: "22x23" or "12*90"
+        /// </summary>
+        /// <param name="dimensionsSeperator">String containing dimensions and seperator</param>
+        public City(string dimensionsSeperator)
+        {
+            // Set default values
+            int width = 50;
+            int height = 50;
+
+            // (?<width>\d{1,})[x\*](?<height>\d{1,})
+            // (?<width>\d{1,})                         Named capture group containing width
+            //                 [x\*]                    Seperator, either 'x' or '*'
+            //                      (?<height>\d{1,})   Named capture group containing height
+            Regex rgxSeperatorString = new Regex(@"(?<width>\d{1,})[x\*](?<height>\d{1,})");
+            Match match = rgxSeperatorString.Match(dimensionsSeperator);
+
+            // If match is successful
+            if(match.Success)
+            {
+                // Try to parse results into ints
+                int.TryParse(match.Groups["width"].Value, out width);
+                int.TryParse(match.Groups["height"].Value, out height);
+            }
+
+            // Set height and width
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ src/City/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("City")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("70148f5b-0977-4a83-a9bf-a6eb8e495558")]

8236b2922f31f4e8e7573066d6509567fbc5613b
Marvin Altemeier <maltemeier@2x4.de>
2016-08-13 15:22:35 +0200

    Added appartment and street

---
+++ src/City/BuildingTypes/Appartment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace City.BuildingTypes
+{
+    class Appartment : Building
+    {
+        public Appartment(int x, int y) : base('O', x, y) { }
+    }
+}
---
+++ src/City/Street.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace City
+{
+    class Street : Square
+    {
+        public Street(int x, int y) : base('#', x, y) { }
+    }
+}

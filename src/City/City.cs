fd880a7296ab7fe990564d1e0bffc14fba99c268
Marvin Altemeier <maltemeier@2x4.de>
2016-08-13 18:02:48 +0200

    Added width and height to city, throwing an exception if a square is out of bounds

--- src/City/City.cs
+++ src/City/City.cs
@@ -11,9 +11,30 @@ namespace City
     public class City : IEnumerable<Square>
     {
         /// <summary>
+        /// Gets the width of the city
+        /// </summary>
+        public int Width { get; private set; }
+
+        /// <summary>
+        /// Gets the height of the city
+        /// </summary>
+        public int Height { get; private set; }
+
+        /// <summary>
         /// List of squares in the city
         /// </summary>
         private readonly List<Square> _squares = new List<Square>();
+
+        /// <summary>
+        /// Constructor, sets maximum bounds
+        /// </summary>
+        /// <param name="width">Width of the City</param>
+        /// <param name="height">Height of the City</param>
+        public City(int width, int height) : base()
+        {
+            Width = width;
+            Height = height;
+        }
 
         /// <summary>
         /// Add new square to the city
@@ -21,7 +42,14 @@ namespace City
         /// <param name="square">Square to add</param>
         public void Add(Square square)
         {
-            _squares.Add(square);
+            if (square.X > Width - 1 || square.Y > Height - 1)
+            {
+                throw new SquareIndexOutOfBoundsException();
+            }
+            else
+            {
+                _squares.Add(square);
+            }
         }
 
         public IEnumerator<Square> GetEnumerator() => _squares.GetEnumerator();

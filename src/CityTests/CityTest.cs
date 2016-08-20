c55e355ee076c9aae0014c100e8845bb3703dcba
Marvin Altemeier <maltemeier@2x4.de>
2016-08-20 13:47:48 +0200

    Added test for debugging

--- src/CityTests/CityTest.cs
+++ src/CityTests/CityTest.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using City;
+using System.Collections.Generic;
 using Xunit;
 
 namespace CityTests
 {
     public class CityTest
     {
-    
+        [Fact]
+        public void CityGeneratorTest()
+        {
+            City.City c = CityGenerator.BuildCity(40, 40);
+            string result = c.ToString();
+            Assert.False(true);
+        }
     }
 }

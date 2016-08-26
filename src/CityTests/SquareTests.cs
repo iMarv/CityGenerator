1f82bc9879369e709cab6c8aecb27db0aed7feef
Marvin Altemeier <marv.internetz@gmail.com>
2016-08-26 20:28:19 +0200

    Added Appartment ToString() test closes #4

---
+++ src/CityTests/SquareTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using City.BuildingTypes;
+using City;
+
+namespace CityTests
+{
+    public class SquareTests
+    {
+        [Fact]
+        public void AppartmentToStringTest()
+        {
+            Appartment a = new Appartment(1, 1, null);
+            Assert.Equal("O", a.ToString());
+        }
+    }
+}
